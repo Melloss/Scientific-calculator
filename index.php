93fa8382c2bf27e0783d8c9d6aafdf33a935f18a
c5rogers <natenelhaylemariam@gmail.com>
2022-12-04 23:25:40 -0800

    this is the file

--- index.php
+++ index.php
@@ -5,8 +5,71 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scientific Calculator</title>
+    <link rel="stylesheet" href="stylesheet/scientficCalculator.css">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="upper">
+            <div class="icon-bar">
+                <div class="icon">
+                    <img src="image/7.png" ><span>Standard</span>
+                </div>
+                <div class="logo">
+                    <img src="image/12.png">
+                </div>
+            </div>
+            <div class="screen">
+
+            </div>
+        </div>
+        <div class="buuton-filed">
+            <div class="function-buttons">
+                <div class="row1">
+                    <button name="mc" class="mini-buttons">MC</button>
+                    <button name="mr" class="mini-buttons">MR</button>
+                    <button name="m+" class="mini-buttons">M+</button>
+                    <button name="m-" class="mini-buttons">M-</button>
+                    <button name="ms" class="mini-buttons">MS</button>
+                    <button name="mc" class="mini-buttons">MC<sup>.</sup></button>
+                </div>
+            </div>
+            <div class="button-area">
+                <div class="operator-button">
+                    <div class="row2">
+                        <button name="modulo" calss="large-button">%</button>
+                        <button name="ce" calss="large-button">CE</button>
+                        <button name="ac" calss="large-button">AC</button>
+                        <button name="division" calss="large-button">/</button>
+                    </div>
+                </div>
+                <div class="number-button">
+                    <div class="row3">
+                        <button name="number" class="large-button">7</button>
+                        <button name="number" class="large-button">8</button>
+                        <button name="number" class="large-button">9</button>
+                        <button name="number" class="large-button">x</button>
+                    </div>
+                    <div class="row4">
+                        <button name="number" class="large-button">4</button>
+                        <button name="number" class="large-button">5</button>
+                        <button name="number" class="large-button">6</button>
+                        <button name="number" class="large-button">-</button>
+                    </div>
+                    <div class="row5">
+                        <button name="number" class="large-button">1</button>
+                        <button name="number" class="large-button">2</button>
+                        <button name="number" class="large-button">3</button>
+                        <button name="number" class="large-button">+</button>
+                    </div>
+                    <div class="row6">
+                        <button name="number" class="large-button"></button>
+                        <button name="number" class="large-button">0</button>
+                        <button name="number" class="large-button">.</button>
+                        <button name="number" class="large-button">=</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
